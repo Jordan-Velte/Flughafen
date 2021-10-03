c2ec16de201ba71c5bfd6276e327982629b6868c
Jordan-Velte <jordan.velte@gymhum.de>
2021-10-03 22:37:47 +0200

    Erstellung der Klassenvariablen, Setter und Getter

--- Flughafen/src/App.java
+++ Flughafen/src/App.java
@@ -1,4 +1,5 @@
 public class App {
+    //Main-Methode als Einstiegspunkt des Programms
     public static void main(String[] args) throws Exception {
         System.out.println("Hallo, liebe Informatiker!");
         System.out.println("Hallo, liebe Informatiker!");
