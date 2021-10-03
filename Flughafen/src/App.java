d5c675b56298d7dd271cab70de8404bd0d7bdfe3
Jordan-Velte <jordan.velte@gymhum.de>
2021-10-03 21:57:37 +0200

    Neue Ausgabe (init)

--- Flughafen/src/App.java
+++ Flughafen/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hallo, liebe Informatiker!");
+        System.out.println("Hallo, liebe Informatiker!");
     }
 }
